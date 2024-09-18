f5dcda5d4f4c07b6a94bdfc1fd04683b6865942f
Thailan Gouvea <thailangm@icloud.com>
2024-09-18 17:23:00 +0300

    Resets basic css on App

--- Fundamentos-React-AT/src/App.jsx
+++ Fundamentos-React-AT/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./components/home/home";
+import Home from "./pages/home/home";
 import Details from "./pages/details/details";
 
 const router = createBrowserRouter([
