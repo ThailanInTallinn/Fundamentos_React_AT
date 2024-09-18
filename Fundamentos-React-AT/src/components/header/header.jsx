ac6b7546844d58b08d1553a67bc6acb5d657acaa
Thailan Gouvea <thailangm@icloud.com>
2024-09-18 17:16:56 +0300

    Removes unnecessary imports

--- Fundamentos-React-AT/src/components/header/header.jsx
+++ Fundamentos-React-AT/src/components/header/header.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./header.module.css";
-
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
-      <h1></h1>
+      <Link to="/">
+        <h1>HotelBooking.</h1>
+      </Link>
+
+      <input type="text" placeholder="Busque seu hotel" />
     </header>
   );
 }
