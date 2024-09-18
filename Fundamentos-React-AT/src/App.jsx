593ce2185b54ec5c497e26c344858e2402377d1c
Thailan Gouvea <thailangm@icloud.com>
2024-09-18 14:16:33 +0300

    Initializes react router dom

--- Fundamentos-React-AT/src/App.jsx
+++ Fundamentos-React-AT/src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
 
 export default function App() {
   return <div className="App"></div>;
