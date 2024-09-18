65735da11cfcdb03e7318d10043f08927d012bd2
Thailan Gouvea <thailangm@icloud.com>
2024-09-18 14:45:37 +0300

    Fixes routing to home page

--- Fundamentos-React-AT/src/App.jsx
+++ Fundamentos-React-AT/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/home/home";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
 
 export default function App() {
   return <div className="App"></div>;
