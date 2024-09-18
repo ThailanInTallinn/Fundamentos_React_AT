9a878e7a6e752a743d5aa98c781da02b73872bcc
Thailan Gouvea <thailangm@icloud.com>
2024-09-18 14:48:54 +0300

    Fixes app component router-wise

--- Fundamentos-React-AT/src/App.jsx
+++ Fundamentos-React-AT/src/App.jsx
@@ -4,6 +4,6 @@ import Home from "./components/home/home";
 
 const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
 
-export default function App() {
-  return <div className="App"></div>;
+export default function Routes() {
+  return <RouterProvider router={router} />;
 }
