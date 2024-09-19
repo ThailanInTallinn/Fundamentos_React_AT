512ebbd977e8e19fc07206dcba7bdc331dd5f39a
Thailan Gouvea <thailangm@icloud.com>
2024-09-19 10:43:37 +0300

    Calls useParams from react-router-dom

--- Fundamentos-React-AT/src/pages/details/details.jsx
+++ Fundamentos-React-AT/src/pages/details/details.jsx
@@ -1,10 +1,14 @@
 import Header from "../../components/header/header";
 import styles from "./details.module.css";
+import { useParams } from "react-router-dom";
 
 export default function Details() {
+  const parametros = useParams();
+
   return (
     <div className={styles.detailsContainer}>
       <Header />
+      <h2>{parametros.id}</h2>
     </div>
   );
 }
