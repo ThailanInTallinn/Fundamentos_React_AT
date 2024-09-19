02cc8fd54c8ad7fa0ba73ef8a466384746729a8d
Thailan Gouvea <thailangm@icloud.com>
2024-09-19 08:49:01 +0300

    Adds an options container component

---
+++ Fundamentos-React-AT/src/components/options/options.jsx
@@ -0,0 +1,5 @@
+import styles from "./options.module.css";
+
+export default function Options() {
+  return <div className={styles.optionsContainer}></div>;
+}
--- Fundamentos-React-AT/src/pages/home/home.jsx
+++ Fundamentos-React-AT/src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/header/header";
+import Options from "../../components/options/options";
 import styles from "./home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <Header />
+      <Options />
     </div>
   );
 }
