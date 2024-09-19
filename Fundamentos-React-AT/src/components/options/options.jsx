44c8adb6fe8d894d49328d62871b69a4d958a7e8
Thailan Gouvea <thailangm@icloud.com>
2024-09-19 08:58:56 +0300

    Makes initial changes on details visible

--- Fundamentos-React-AT/src/components/options/options.jsx
+++ Fundamentos-React-AT/src/components/options/options.jsx
@@ -1,5 +1,14 @@
 import styles from "./options.module.css";
 
 export default function Options() {
-  return <div className={styles.optionsContainer}></div>;
+  return (
+    <div className={styles.optionsContainer}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia architecto
+        quam rerum ratione nostrum fugit, blanditiis itaque nobis magnam quasi
+        beatae ullam cupiditate asperiores ipsum doloremque, voluptas alias
+        molestiae facilis?
+      </p>
+    </div>
+  );
 }
