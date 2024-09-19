c89e1b1434f4d9f6a9d5a7a9ed74bfcc4941ff6b
Thailan Gouvea <thailangm@icloud.com>
2024-09-19 11:56:37 +0300

    Adds a button for adding new hotels

--- Fundamentos-React-AT/src/pages/home/home.jsx
+++ Fundamentos-React-AT/src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import Body from "../../components/body/body";
 import Header from "../../components/header/header";
 import Options from "../../components/options/options";
 import styles from "./home.module.css";
+import { IoMdAdd } from "react-icons/io";
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
       <Header />
       <Options />
       <Body />
+      <span className={styles.addButton}>
+        <IoMdAdd />
+      </span>
     </div>
   );
 }
