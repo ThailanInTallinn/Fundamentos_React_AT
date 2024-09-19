7d1ad25f35db1c382cdc203680c6c3b26573c0fb
Thailan Gouvea <thailangm@icloud.com>
2024-09-19 10:01:13 +0300

    Turns the cards components into links to details

--- Fundamentos-React-AT/src/components/card/card.jsx
+++ Fundamentos-React-AT/src/components/card/card.jsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 import styles from "./card.module.css";
 
 export default function Card(props) {
   return (
-    <div className={styles.cardContainer}>
-      <img src={props.img} />
-      <p>{props.name}</p>
-      <p>{props.price}</p>
-      <p>
-        {props.city}, {props.state}
-      </p>
-      <p>Nota: {props.score}</p>
-    </div>
+    <Link to="/details">
+      <div className={styles.cardContainer}>
+        <img src={props.img} />
+        <p>{props.name}</p>
+        <p>{props.price}</p>
+        <p>
+          {props.city}, {props.state}
+        </p>
+        <p>Classificação: {props.score}</p>
+      </div>
+    </Link>
   );
 }
