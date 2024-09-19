cc5eef89d3896d2d55d841835663b09688f2f6cc
Thailan Gouvea <thailangm@icloud.com>
2024-09-19 09:51:02 +0300

    Styles body container and card container
    It's been createde and called a function to create mock components
    on Body, but it'll be deleted later on.

--- Fundamentos-React-AT/src/components/body/body.jsx
+++ Fundamentos-React-AT/src/components/body/body.jsx
@@ -1,5 +1,24 @@
 import styles from "./body.module.css";
+import Card from "../card/card";
 
 export default function Body() {
-  return <div className={styles.bodyContainer}></div>;
+  const hotelList = [];
+
+  function multiHotel() {
+    for (let i = 0; i < 10; i++) {
+      hotelList.push(
+        <Card
+          img="https://mocks.elevenplusexams.co.uk/assets/uncompiled/online_mocks1.jpg?t=3654667474"
+          name="Hotel"
+          price="300"
+          city="Rio de Janeiro"
+          state="RJ"
+          score="5"
+        />
+      );
+    }
+
+    return hotelList;
+  }
+  return <div className={styles.bodyContainer}>{multiHotel()};</div>;
 }
