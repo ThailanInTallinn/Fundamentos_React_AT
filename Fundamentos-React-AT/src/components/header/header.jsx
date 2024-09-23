5cab6f6e2458de7b7f604773c70dfaf5f34ad4af
Thailan Gouvea <thailangm@icloud.com>
2024-09-23 12:51:38 +0300

    Makes the home page responsible

--- Fundamentos-React-AT/src/components/header/header.jsx
+++ Fundamentos-React-AT/src/components/header/header.jsx
@@ -1,14 +1,28 @@
 import { Link } from "react-router-dom";
 import styles from "./header.module.css";
 import { CiLight, CiDark } from "react-icons/ci";
+import { useState } from "react";
 
 export default function Header({ searchItems, setTheme, themeOption }) {
+  const [menuToggle, setMenuToggle] = useState(false);
+
   return (
     <header className={styles.headerContainer}>
       <Link to="/">
         <h1>HotelBooking.</h1>
       </Link>
-      <div className={styles.optionsContainer}>
+      <span
+        className={styles.menuToggle}
+        onClick={() => {
+          setMenuToggle(!menuToggle);
+        }}
+      >
+        =
+      </span>
+      <div
+        className={styles.optionsContainer}
+        style={{ display: menuToggle ? "flex" : "" }}
+      >
         <input
           type="text"
           placeholder="Busque seu hotel"
